085048a98d2b2eca0c9205ac430e974dc834673a
Júlio Garavelli <juliocgaravelli@gmail.com>
2017-06-26 14:42:03 -0300

    Commit inicial: Projeto criado + arquitetura básica MVP + cocoapods.

---
+++ LancheTop/LancheTop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LancheTop
+//
+//  Created by Júlio César Garavelli on 26/06/17.
+//  Copyright © 2017 Júlio Garavelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
